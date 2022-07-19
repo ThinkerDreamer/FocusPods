cdc6f4f444e46cbe0fad31c3402a877b437b9a52
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2022-07-19 18:23:50 -0400

    refactor to add SQLAlchemy

--- server/focus_pods.py
+++ server/focus_pods.py
@@ -1,6 +1,32 @@
-from flask import Flask, redirect, url_for, render_template, request
+from curses import meta
+from enum import unique
+import os
+from flask import Flask, flash, redirect, session, url_for, render_template, request, session
+from markupsafe import Markup
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://{os.environ["DB_USERNAME"]}:{os.environ["DB_PASSWORD"]}@localhost/focus_pods_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
+    
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r}, email={self.email!r}, created_at={self.created_at!r})"
+
+@app.route("/users/")
+def get_users():
+    users = User.query.all()
+    return render_template("users.html", users=users)
 
 @app.route("/")
 def home():
@@ -12,15 +38,38 @@ def logged_in(name):
 
 @app.route("/sign-up/", methods=["POST", "GET"])
 def sign_up():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+        user = User(name=name,
+                    email=email,
+                    password=password,
+                    )
+        db.session.add(user)
+        db.session.commit()
+        print(f'{user} added')
+        flash(f'Welcome {name}!')
+        flash("You have successfully signed up!")
+        flash(Markup("Please <a href='/login'>login</a> to continue"))
     return render_template("signup.html")
  
 @app.route("/login/", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        user = request.form["name"]
-        return redirect(url_for("logged_in", name=user))
+        user = request.form["username"]
+        password = request.form["password"]
+        if True:
+            session["logged_in"] = True
+            return redirect(url_for("logged_in", name=user))
+            
+        else:
+            flash("Incorrect login credentials!")
+            return render_template("login.html")
     else:
         return render_template("login.html")
  
 if __name__ == "__main__":
+    app.secret_key = "I am a super secret key!"
+    app.config["SESSION_TYPE"] = 'filesystem'
     app.run(debug=True)
