332045d526365b7c1257d9f1b613182dbfa68aad
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2022-07-28 10:23:58 -0400

    remove old models code

--- server/focus_pods.py
+++ server/focus_pods.py
@@ -2,46 +2,15 @@ import os
 from flask import Flask, flash, redirect, session, url_for, render_template, request, session
 from markupsafe import Markup
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, ForeignKey, Table, select
-from sqlalchemy import func
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import declarative_base
+
+from lib.models import User
 
 Base = declarative_base()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://{os.environ["DB_USERNAME"]}:{os.environ["DB_PASSWORD"]}@localhost/focus_pods_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
-association_table = Table(
-    "rooms_users",
-    Base.metadata,
-    Column("room_id", ForeignKey("rooms.id")),
-    Column("user_id", ForeignKey("users.id")),
-)
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(100), nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True),
-                           server_default=func.now())
-    children = relationship("Room", secondary=association_table)
-
-    def __repr__(self) -> str:
-        return f"User(id={self.id!r}, name={self.name!r}, email={self.email!r}, created_at={self.created_at!r})"
-
-class Room(db.Model):
-    __tablename__ = 'rooms'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100))
-    created_at = db.Column(db.DateTime(timezone=True),
-                           server_default=func.now())
-
-    def __repr__(self) -> str:
-        return f"Room(id={self.id!r}, name={self.name!r}, created_at={self.created_at!r})"
-
 
 @app.route("/users/")
 def get_users():
@@ -64,12 +33,12 @@ def sign_up():
         password = request.form["password"]
         user = User(name=name,
                     email=email,
-                    password=password,
-                    created_at= func.now(),
+                    password=password
                     )
         db.session.add(user)
         db.session.commit()
         print(f'{user} added')
+        print(f'user.password is {user.password}')
         flash(f'Welcome {name}!')
         flash("You have successfully signed up!")
         flash(Markup("Please <a href='/login'>login</a> to continue"))
@@ -78,13 +47,15 @@ def sign_up():
 @app.route("/login/", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        username = request.form["username"]
+        email = request.form["email"]
         password = request.form["password"]
-        result = db.session.execute(select(User).where(User.email==username).where(User.password==password)).all()
+        print(db.session)
+        result = db.session.execute(select(User).where(User.email==email).where(User.password==password)).all()
         print("do we see this?", result)
         if result:
+            # TODO: Get user's name from the db
             session["logged_in"] = True
-            return redirect(url_for("logged_in", name=username))
+            return redirect(url_for("logged_in", name=email))
 
         else:
             flash("Incorrect login credentials!")
