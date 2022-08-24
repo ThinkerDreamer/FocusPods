6fe922490fbe0251d95bf4e3f8c1bd8e402cb2be
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2022-08-24 13:32:48 -0400

    correct minor error in print stmnt

--- server/__init__.py
+++ server/__init__.py
@@ -4,13 +4,14 @@ def start_app():
     """Initialize the core application."""
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
-    print("I am being called from inside init_app in __init__.py")
+    print("I am being called from inside start_app in __init__.py")
 
     from .lib.models import db
     db.init_app(app)
 
     with app.app_context():
         # Include our Routes
+        print("I'm inside the app_context!")
         from . import routes
         print(routes)
         return app
