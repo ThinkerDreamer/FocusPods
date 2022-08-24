6ca3789c0720178c703bdf34d234a90e36ca820d
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2022-08-24 09:03:48 -0400

    add debugging stmnts

--- server/__init__.py
+++ server/__init__.py
@@ -4,13 +4,15 @@ def init_app():
     """Initialize the core application."""
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
-    
+    print("I am being called from inside init_app in __init__.py")
+
     from .lib.models import db
     db.init_app(app)
 
     with app.app_context():
         # Include our Routes
         from . import routes
+        print(routes)
         return app
 
 # app = Flask(__name__)
