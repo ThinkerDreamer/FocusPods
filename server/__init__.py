fae46935701d08b69fc1e64d6eb89a4a7fe25858
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2022-08-23 21:02:39 -0400

    refactor init

--- server/__init__.py
+++ server/__init__.py
@@ -1,10 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
+db = SQLAlchemy()
+
+def init_app():
+    """Initialize the core application."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+
+    with app.app_context():
+        # Include our Routes
+        from . import routes
+        return app
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
-db = SQLAlchemy(app)
+
 
 
 if __name__ == "__main__":
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
-from server.app import app
+from server import init_app
+
+app = init_app()
 
 if __name__ == "__main__":
         app.run()
