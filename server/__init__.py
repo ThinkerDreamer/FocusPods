a00c0e13f66120f0a72a73cfc7b38a1b1f35faa3
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2022-11-17 00:43:00 +0100

    minor formatting edit

--- server/__init__.py
+++ server/__init__.py
@@ -1,17 +1,21 @@
 from flask import Flask
 
+
 def start_app():
     """Initialize the core application."""
+
     app = Flask(__name__)
-    app.config.from_pyfile('config.py')
+    app.config.from_pyfile("config.py")
     print("I am being called from inside start_app in __init__.py")
 
     from .lib.models import db
+
     db.init_app(app)
 
     with app.app_context():
         # Include our Routes
         print("I'm inside the app_context!")
         from . import routes
+
         print(routes)
         return app
