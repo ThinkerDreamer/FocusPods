4ab1a4393206060e5977378ab6c1cb1d7e70a1e7
Angel H <74881094+ThinkerDreamer@users.noreply.github.com>
2023-01-13 15:59:08 +0100

    add comment about purpose and use

--- scripts/db-up.sh
+++ scripts/db-up.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
+# This starts postgres on Ubuntu over Windows Subsystem for Linux, on my machine anyway.
 unshare --pid --fork --mount-proc /lib/systemd/systemd --system-unit=basic.target &
 sleep 5
 nsenter --all -t "$(pgrep -xo systemd)" runuser -P -l "$USER" -c "exec $SHELL"
