6ac8a1da9c141fad7c4bac6dcb80b11d402b01a4
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-01-13 16:18:04 +0100

    lint quotes, comment out old database info

--- server/config.py
+++ server/config.py
@@ -3,18 +3,18 @@ from os import environ, path
 from dotenv import load_dotenv
 
 basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, '.env'))
+load_dotenv(path.join(basedir, ".env"))
 
-DB_USERNAME = environ.get('DB_USERNAME')
-DB_PASSWORD = environ.get('DB_PASSWORD')
-DATABASE_URL = environ.get('DATABASE_URL')
-SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@localhost/focus_pods_db'
-SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+DB_USERNAME = environ.get("DB_USERNAME")
+DB_PASSWORD = environ.get("DB_PASSWORD")
+# DATABASE_URL = environ.get('DATABASE_URL')
+# SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@localhost/focus_pods_db'
+SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL").replace(
+    "postgres://", "postgresql://", 1
+)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 TESTING = True
 DEBUG = True
-FLASK_ENV = 'development'
-SECRET_KEY = environ.get('SECRET_KEY')
-SESSION_TYPE = 'filesystem'
-
-
+# FLASK_ENV = 'development'
+SECRET_KEY = environ.get("SECRET_KEY")
+# SESSION_TYPE = 'filesystem'
